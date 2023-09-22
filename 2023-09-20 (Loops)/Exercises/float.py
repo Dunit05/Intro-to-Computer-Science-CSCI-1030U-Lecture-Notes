18d003da365b86e0484907aaa3ac8808616397b2
Tommy <tommy.05.tdm@gmail.com>
2023-09-22 12:44:09 -0400

    finished float.py

--- 2023-09-20 (Loops)/Exercises/float.py
+++ 2023-09-20 (Loops)/Exercises/float.py
@@ -19,3 +19,17 @@ for n in range(1, 50):
     sin += (-1) ** n * x ** (2 * n + 1) / math.factorial(2 * n + 1)
 
 print(f"sin({x}) is approximately {sin}")
+
+# Write the code which, given a positive floating point number x, returns a value that is close to the square root of x
+# Method:
+# Guess any square root estimate
+# Determine if the square of that estimate is too high or too low
+# Add or subtract from that estimate to bring it closer to the right answer
+
+x = float(input("Enter a positive floating point number: "))
+guess = x / 2
+
+for n in range(1, 10):
+    guess = (guess + x / guess) / 2
+
+print(f"The square root of {x} is approximately {guess}")
